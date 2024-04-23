0d1dd7629ccd198ee86ac81793547e24a5182c95
Stefano <snebo54@hotmail.com>
2024-04-23 23:28:36 +0100

    add new and create actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,22 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path, notice: "Posted!"
+    else
+      render :new, status: :unprocessable_entity, notice: "failed to create post"
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
